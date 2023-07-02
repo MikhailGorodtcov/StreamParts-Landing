57b9b00f2e9cf8cd7fd0decee7e4bc70e79cd5c6
Mikhail <miwustrilo@gmail.com>
2023-07-02 15:16:25 +0300

    Добавляет нижнюю часть Header 768-320

--- src/js/components/menu.js
+++ src/js/components/menu.js
@@ -1,14 +1,14 @@
-const MENU_BURGER_EL_ID = 'menu-burger';
-const MENU_EL_ID = 'menu';
+// const MENU_BURGER_EL_ID = 'menu-burger';
+// const MENU_EL_ID = 'menu';
 
-export default class Menu {
-  constructor(menuBurgerEltId = MENU_BURGER_EL_ID, menuElId = MENU_EL_ID) {
-    this.menuBurger = document.getElementById(menuBurgerEltId);
-    this.menu = document.getElementById(menuElId);
+// export default class Menu {
+//   constructor(menuBurgerEltId = MENU_BURGER_EL_ID, menuElId = MENU_EL_ID) {
+//     this.menuBurger = document.getElementById(menuBurgerEltId);
+//     this.menu = document.getElementById(menuElId);
 
-    this.menuBurger.addEventListener('click', (event) => {
-      this.menuBurger.classList.toggle('open');
-      this.menu.classList.toggle('open');
-    });
-  }
-};
+//     this.menuBurger.addEventListener('click', (event) => {
+//       this.menuBurger.classList.toggle('open');
+//       this.menu.classList.toggle('open');
+//     });
+//   }
+// };
