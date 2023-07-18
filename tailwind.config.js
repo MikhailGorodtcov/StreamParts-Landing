415686adf45b57b248499308253baeee66e6eae7
sshurkhovetskii <sshurkhovetskii@gmail.com>
2023-07-18 13:41:14 +0200

    fix h2 & some tags

--- tailwind.config.js
+++ tailwind.config.js
@@ -33,12 +33,6 @@ module.exports = {
         mxlg: "202px",
       },
       fontFamily: {
-        // sans: [
-        //   'SF Pro Display',
-        //   'SF Pro Text',
-        //   'system-ui',
-        //   '-apple-system',
-        // ],
         pro: "SF Pro Display",
         protext: "SF Pro Text",
         mono: "Roboto Mono, sans-serif",
